936bcbf596e727491e808534b0eeb26499b786bb
FatmaElMahdi1000 <f69246984@gmail.com>
2023-10-10 16:24:30 -0700

    a

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include"main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Description: a function that prints 10 times the alphabet
+ *
+ * Return: void
+*/
+void print_alphabet_x10(void)
+{
+	int c, j;
+
+	for (c = 0, c <= 9,  c++);
+	{
+		int j;
+
+		for (j = 'a', j <= 'z', j++);
+		{
+			_putchar("j");
+
+		}
+		_putchar("\n");
+	}
+}
