cbe47723cca5dfc2e28dde4dff687f8d474ccc99
FatmaElMahdi1000 <f69246984@gmail.com>
2023-10-10 16:00:40 -0700

    a

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point 
+ *
+ * description: a program to print in lower case
+ *
+ * Return: Always 0 (success)
+*/
+void print_alphabet(void)
+{
+	int c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+	return (0);
+}
