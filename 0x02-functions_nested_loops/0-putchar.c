9fc81c038f23eaf9b4f12238c30a60a0e4f90a37
FatmaElMahdi1000 <f69246984@gmail.com>
2023-10-10 14:01:15 -0700

    a

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A program to print putchar
+ *
+ * Return: Alway 0 (success)
+*/
+int main(void)
+{
+	int i = 0, j;
+
+	while (i < 1)
+	{
+		int j = 0;
+		
+		while (j < 1)
+		{
+			printf("_putchar");
+			j++;
+		}
+		printf("\n");
+		i++;
+	}
+return (0);
+}
+
