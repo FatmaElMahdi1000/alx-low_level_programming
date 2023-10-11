809df0267b8164abfffb8a0ef21d16504309e6d2
FatmaElMahdi1000 <f69246984@gmail.com>
2023-10-11 03:23:39 -0700

    a

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print all possible different comb.
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	int i = 0, j;
+
+	while (i <= 9)
+	{
+		int j = 0;
+
+		while (j <= 9)
+		{
+			if (i != j && i < j)
+			{
+				putchar(j + '0');
+				putchar(i + '0');
+
+				if (i + j != 17)
+				{
+					putchar (',');
+					putchar (' ');
+				}
+			}
+			i++;
+		}
+		j++;
+	}
+	putchar('\n');
+
+	return (0);
+}
