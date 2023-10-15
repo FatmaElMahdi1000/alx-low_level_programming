d0030fdc584ab4bb1929c1c800958d32169b63ae
FatmaElMahdi1000 <f69246984@gmail.com>
2023-10-15 02:35:50 -0700

    a

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_diagonal - draws a diagonal line
+ *
+ * @n: number of times the character \ should be printed
+*/
+void print_diagonal(int n)
+{
+	int space, mychar;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (space = 1; space <= n; space++)
+		{
+			for (mychar = 1; mychar <= space; mychar++)
+			{
+				_putchar(' ');
+			}
+			_putchar('/');
+		}
+		_putchar('\n');
+	}
+
+}
