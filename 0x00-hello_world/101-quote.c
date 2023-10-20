809316ef994f9f918d70662cb31559c501d53ef7
FatmaElMahdi1000 <f69246984@gmail.com>
2023-10-20 13:23:52 -0700

    a

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,16 @@
 #include <unistd.h>
 /***
- * main - Entry point
+ * @main - Entry point
  *
  * Description: print a code using right functions
  *       write(int fd, const void *buf, size_t count)
  *
  * Return: 1 (not success)
  */
-int main (void)
+int main(void)
 {
 	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	
-	write (1, quo,59);
+	write(1, quo ,59);
 	return (1);
 }
