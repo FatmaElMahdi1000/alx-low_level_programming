2206b9135114a1e85d60435972041a86161276a5
FatmaElMahdi1000 <f69246984@gmail.com>
2023-10-23 14:50:28 -0700

    a

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,17 +2,17 @@
 
 /**
  *  _memset - memory is filled with constant type
- *
  *  @s: pointer
  *  @b: constant
  *  @n: the bytes
  *  Return: pointer
 */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int itr;
 
-	for (itr = 0; n > 0; itr++; n--)
+	for (itr = 0; n > 0; itr++, n--)
 	{
 		s[itr] = b;
 	}
