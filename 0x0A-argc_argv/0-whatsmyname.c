8fa150d597e3b19f707aac791b6ca689a40f1c16
FatmaElMahdi1000 <f69246984@gmail.com>
2023-10-28 09:35:41 -0700

    a

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ * main - Entry point
+ *
+ * Description: prints a name
+ * @argc: count
+ * @argv: strings
+ *
+ * Return: 0 (success)
+*/
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
