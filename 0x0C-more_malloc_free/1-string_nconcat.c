3ff7002d94c6f9428ef26e96d15aa04f0ca294f8
FatmaElMahdi1000 <f69246984@gmail.com>
2023-11-02 15:55:16 -0700

    a

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,52 @@
+#include"main.h"
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * string_nconcat - point to a newly allocated space in memory
+ *
+ * @s1: value
+ * @s2: Value
+ * @n: int
+ * Return: pointer
+*/
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *str;
+
+	unsigned int i;
+	unsigned int j; 
+	unsigned int s1_length; 
+	unsigned int s2_length;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (s1_length = 0; s1[s1_length] != '\0'; s1_length++)
+		;
+	for (s2_length = 0; s2[s2_length] != '\0'; s2_length++)
+		;
+
+	str = malloc(s1_length + n + 1);
+
+
+	j = malloc(sizeof(s2[n]));
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; s1[i] != '\0'; i++)
+		str[i] = s1[i];
+
+	for (j = 0; j < n; j++)
+	{
+		str[i] = s2[j];
+		i++;
+	}
+	str[i] = '\0';
+		return (str);
+}
