3b45f19d680dfd4fbdd7b785d05ac91e28870665
FatmaElMahdi1000 <f69246984@gmail.com>
2023-11-02 16:35:33 -0700

    a

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,39 @@
+#include"main.h"
+
+/**
+ * *_memset - fills memory
+ * @s: pointer
+ * @b: constant
+ * @n: max
+ * Return: s
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *ptr = s;
+
+	while (n--)
+		*s++ = b;
+
+	return (ptr);
+}
+/**
+ * *_calloc - allocates memory
+ * @nmemb: array
+ * @size: size
+ * Return: pointer
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *m;
+
+	if (size == 0 || nmemb == 0)
+		return (NULL);
+	m = malloc(sizeof(int) * nmemb);
+
+	if (m == 0)
+		return (NULL);
+	_memset(m, 0, sizeof(int) * nmemb);
+
+	return (m);
+}
