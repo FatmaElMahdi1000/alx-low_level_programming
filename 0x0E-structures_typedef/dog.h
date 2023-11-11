ba13aafc1ba2a88f5e5a5cde65db0d4c30e2b26a
FatmaElMahdi1000 <f69246984@gmail.com>
2023-11-11 07:27:01 -0800

    a

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name
+ *
+ * Description: nothing
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
