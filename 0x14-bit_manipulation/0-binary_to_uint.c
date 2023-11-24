bea224476233904b7dd72de974317143f1789182
FatmaElMahdi1000 <f69246984@gmail.com>
2023-11-24 15:27:12 -0800

    a

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,24 +2,25 @@
 
 /**
  * binary_to_uint - function
+ * atoi - conversion
  * @b: int
- *Return 0 or int
- *
+ *Return: 0 or int
 */
 
+int atoi(const char *b);
 unsigned int binary_to_uint(const char *b)
 {
-        unsigned int num = 0;
+	unsigned int num = 0;
 
-        if (!b)
-                return (0);
+	if (!b)
+		return (0);
+	while (*b)
+{
+		if (*b != '0' && *b != '1')
+			return (0);
 
-        while (*b)
-        {
-                if (*b != '0' && *b != '1')
-                        return (0);
-                num = num * 2 + (*b++ - '0');
-        }
-        return (num);
+		num = atoi(b);
+}
+	return (num);
 
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <limits.h>
 
+int atoi(const char *b)
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
