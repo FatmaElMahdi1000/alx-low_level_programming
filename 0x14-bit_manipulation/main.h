0755456f42dd9e7bf32a6a6937094b5bee086f8a
FatmaElMahdi1000 <f69246984@gmail.com>
2023-11-25 15:07:38 -0800

    a

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,13 +3,6 @@
 
 #include<stdlib.h>
 #include<stdio.h>
-#include<limits.h>
-
-/*
- * File: main.h
- * Desc: Header file containing prototypes for all functions
- *       written in the 0x13-bit_manipulation directory.
- */
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
