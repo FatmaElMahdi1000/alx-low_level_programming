ad4123e6cfce04e1ec16a28c9c7e649dc4c8cb4f
FatmaElMahdi1000 <f69246984@gmail.com>
2023-11-25 15:06:31 -0800

    a

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,18 +1,16 @@
-#ifdef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h> 
-#include <stdlib.h>
-#include <limits.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<limits.h>
 
 /*
  * File: main.h
- * AUth: Brennan D Baraban
  * Desc: Header file containing prototypes for all functions
  *       written in the 0x13-bit_manipulation directory.
  */
 
-int atoi(const char *b)
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
@@ -20,5 +18,6 @@ int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
 
-#endif
+#endif /* MAIN_H */
