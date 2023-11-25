21db30d10839b7b926f0b5343497d7fe65f9b12d
FatmaElMahdi1000 <f69246984@gmail.com>
2023-11-24 16:17:54 -0800

    a

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,6 +5,13 @@
 #include <stdlib.h>
 #include <limits.h>
 
+/*
+ * File: main.h
+ * AUth: Brennan D Baraban
+ * Desc: Header file containing prototypes for all functions
+ *       written in the 0x13-bit_manipulation directory.
+ */
+
 int atoi(const char *b)
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
