6ffdd9ccc4eac36b8fa7a7a6d19498580f9ca535
FatmaElMahdi1000 <f69246984@gmail.com>
2023-11-27 16:56:47 -0800

    a

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,35 +1,28 @@
-#include"main.h"
-#include <stdlib.h>
+#include "main.h"
 
 /**
- *read_textfile - function
- *@letters: size
- *@filename: string
- *Return: cannot open or NULL: 0
-*/
-
+ * read_textfile - Reads a text file
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters the
+ *           function should read and print.
+ *
+ * Return: If the function fails or filename is NULL - 0.
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	size_t o, r, w;
-
+	ssize_t o, r, w;
 	char *buffer;
 
 	if (filename == NULL)
-	{
 		return (0);
-	}
 
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
-	{
 		return (0);
-	}
 
-	o = open("filename.txt", O_RDONLY);
-
-	r = read(o, buffer, sizeof(buffer));
-
-	w = write(STDOUT_FILENO, buffer, sizeof(buffer) - 1);
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
 
 	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
