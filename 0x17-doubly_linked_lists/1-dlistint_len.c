735d744c6a69e65df5dc288bace95c0187ef7647
FatmaElMahdi1000 <f69246984@gmail.com>
2024-01-27 08:15:33 -0800

    a

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,23 @@
+#include"lists.h"
+
+/**
+ * dlistint_len - counting number of nodes in a linked list
+ *
+ *@h: number of nodes
+ *
+ *Return: number of elements
+ *
+**/
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t counting = 0;
+
+	while (h)
+	{
+		printf("%d\n", counting /**data**/);
+		h = h->next;
+		counting += 1;
+	}
+	return (counting);
+}
