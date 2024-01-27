11ac7eed18a3b5acbf0f09bfb4b7865eb7c8dcc9
FatmaElMahdi1000 <f69246984@gmail.com>
2024-01-27 08:51:18 -0800

    a

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ *add_dnodeint - for adding node at the beg.
+ *
+ * @head: head of the linked list
+ *
+ * @n: data of the node
+ *
+ * Return: Head
+**/
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *temp = malloc(sizeof(dlistint_t));
+
+	if (temp == NULL)
+	{
+		return (NULL);
+	}
+
+	temp->prev = NULL;
+	temp->n = n;
+	temp->next = NULL;
+	temp->next = *head;
+	(*head)->prev = temp;
+	*head = temp;
+	return (*head);
+}
