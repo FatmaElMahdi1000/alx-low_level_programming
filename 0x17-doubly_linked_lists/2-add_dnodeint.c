cf40b3621e60351d646a9a6d1034eccdedfb036c
FatmaElMahdi1000 <f69246984@gmail.com>
2024-01-27 12:34:33 -0800

    a

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -12,16 +12,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *temp = malloc(sizeof(dlistint_t));
 
-	if (temp == NULL)
-	{
-		return (NULL);
-	}
-
 	temp->prev = NULL;
 	temp->n = n;
 	temp->next = NULL;
 
-	if (*head == NULL)
+	if (temp == || *head == NULL)
 	{
 		return (NULL);
 	}
@@ -30,7 +25,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		temp->next = *head;
 		*head->prev = temp;
 		head = temp;
-
 	}
 	return (*head);
+	}
 }
