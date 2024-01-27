52a089d398916becf2030d2676a073fb29ba6d54
FatmaElMahdi1000 <f69246984@gmail.com>
2024-01-27 07:44:25 -0800

    a

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include<lists.h>
+/**
+ *print_dlistint - counting the number of a node in a list
+ *
+ * @h: pointer used to access the list
+ *
+ * Return: counting
+**/
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t counting = 0;
+
+	while (h != NULL)
+	{
+		printf("%d ", h->n);
+		h = h->next;
+		counting += 1;
+	}
+	return (counting);
+
+}
