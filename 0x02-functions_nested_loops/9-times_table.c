9f003cf92a5d63335672844251bf601226191636
FatmaElMahdi1000 <f69246984@gmail.com>
2024-08-17 10:32:07 -0700

    a

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,9 +10,9 @@ void times_table(void)
 {
 	int i, int j, int k;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i < 10 ; i++)
 	{
-		for (j = 0; j < 9; j++)
+		for (j = 0; j < 10; j++)
 		{
 			k = i * j;
 
@@ -33,6 +33,7 @@ void times_table(void)
 			{
 				printf(" %d", k)
 			}
+		}
 			printf(" \n");
 		}
 	}
