6df2dfce208dff6ca48d7c92d064f2b25c089abf
FatmaElMahdi1000 <f69246984@gmail.com>
2024-08-17 10:16:55 -0700

    a

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,33 +8,30 @@
 */
 void times_table(void)
 {
-	int i, int j, int k;
+	int i;
+	int j;
+	int k;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 9; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = 0; j < 9; j++)
 		{
-			k = i * j;
-
-			if (j > 0)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-
-			if (k < 10)
-
-			{
-				_putchar(' ');
-				_putchar(k + '0');
-
-			}
-			else
-			{
-				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
-			}
+		k = i * j;
+		if (j > 0)
+		{
+			printf(",");
 		}
-		_putchar('\n');
-	}
+		if (k < 10)
+		{
+			printf("  %d", k);
+		}
+		else if (k > 10)
+		{
+			printf(" %d", k);
+			else if (k == 10)
+			{
+				printf(" %d", k)
+			}
+			printf(" \n");
+		}
 }
