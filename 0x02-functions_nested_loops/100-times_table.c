5d29294ab673a1fb28afa972a6f504fa3609b1c0
FatmaElMahdi1000 <f69246984@gmail.com>
2024-08-17 12:58:23 -0700

    a

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * print_times_table - creating a table
+ * @n: table size
+ *
+ * Return: a tablen
+**/
+void print_times_table(int n)
+{
+	int i, int j, int k;
+
+	if (n > 15 || n < 0)
+	{
+		fprintf(stderr, "ERROR\n");
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+		{
+			k = j * i;
+
+			if (j > 0)
+			{
+				printf(" ,");
+			}
+
+			if (k < 10)
+			{
+				printf("   %d", k);
+			}
+			else if (k >= 10 && k < 100)
+			{
+				printf("  %d", k);
+			}
+			else if (k >= 100)
+			{
+				printf(" %d", k);
+			}
+		}
+			printf(" \n");
+		}
+	}
+}
