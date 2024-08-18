bada93c1dbf917a9b1607ea6c65afd5f484ad3d8
FatmaElMahdi1000 <f69246984@gmail.com>
2024-08-18 04:47:37 -0700

    a

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,59 @@
+#include "main.h"
+
+/**
+ * fc - for fibonacci
+ *
+ *@n: Length
+ *
+ * Return: fibonacci sequence
+*/
+
+int fc(int n)
+{
+	int i;
+	int sum;
+	int number1 = 1;
+	int number2 = 2;
+
+	if (n <= 0)
+	{
+		fprintf(stderr, "ERROR\n");
+	}
+	printf("%d, %d, ", number1, number2);
+
+	for (i = 2; i < n; i++)
+	{
+		sum = number1 + number2;
+		printf("%d", sum);
+
+		number1 = number2;
+
+		number2 = sum;
+
+		if (i != n - 1)
+		{
+			printf(", ");
+		}
+		else
+		{
+			printf("\n");
+		}
+	}
+	return (0);
+}
+
+/**
+ *main - Entry point
+ *
+ * Description: seq.
+ * Return: 0 success
+**/
+
+int main(void)
+{
+	int n = 51;
+
+	fc(n);
+
+	return (0);
+}
