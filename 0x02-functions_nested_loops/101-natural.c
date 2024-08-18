00a256c642ff9922a81e7c66dbda0b31a9f8b8d9
FatmaElMahdi1000 <f69246984@gmail.com>
2024-08-18 00:25:27 -0700

    a

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * multiples - Getting the sum of multiples
+ *
+ * @n: user input
+ *
+ * Return: multiples of 3 or 5
+**/
+
+int multiples(int n)
+{
+	int i;
+	int sum = 0;
+
+	if (n <= 0)
+	{
+		fprintf(stderr, "ERROR\n");
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			if (i % 3 == 0 || i % 5 == 0)
+			{
+				sum += i;
+			}
+		}
+		printf("%d\n", sum);
+	}
+	return (0);
+}
